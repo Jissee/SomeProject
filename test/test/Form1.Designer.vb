104f73e7277c0ddd5484ba906c57d492c97aa4c7
俣桁 孙 <805084485@qq.com>
2020-08-29 20:39:45 +0800

    upd

--- test/test/Form1.Designer.vb
+++ test/test/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,41 +20,88 @@ Partial Class Form1
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(37, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(55, 12)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(257, 21)
         Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(93, 89)
+        Me.Button1.Location = New System.Drawing.Point(35, 182)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(154, 49)
+        Me.Button1.Size = New System.Drawing.Size(96, 49)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(37, 56)
+        Me.TextBox2.Location = New System.Drawing.Point(55, 39)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(257, 21)
         Me.TextBox2.TabIndex = 1
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "psw"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "name"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(204, 182)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 49)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(29, 113)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(270, 21)
+        Me.ProgressBar1.TabIndex = 6
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(334, 158)
+        Me.ClientSize = New System.Drawing.Size(334, 243)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -68,4 +115,9 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
 End Class
--- test/test/Form1.vb
+++ test/test/Form1.vb
@@ -1,37 +1,246 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim codes(61) As String
-        Dim process(5), posnow As ULong
-        Dim tmp As Integer
-        Dim src, str, sfinal As String
-        posnow = 1
-        str = ""
-        sfinal = ""
-        For i = 0 To 25
-            codes(i) = Chr(i + 65)
-            codes(i + 26) = Chr(i + 97)
+        Dim fname_p, fname_s, psw, pswhash, name As String
+        Dim chasc, rndnum(8), pswlen, byte1(7), byte2(7), byte3(7), byte4(7), binval, proc, pospsw, poshash, rndpos As Byte
+        Dim fileleng As Long
+        Dim binstr As String
+        Dim mhash As New myHash.myHash
+        Randomize()
+        For i = 1 To 8
+            rndnum(i) = Int(Rnd() * 256)
         Next
-        For i = 0 To 9
-            codes(i + 52) = CStr(i)
-        Next
-        src = TextBox1.Text
-        For i = 1 To 1000
-            For j = 1 To 4
-                process(j) = process(j + 1)
-            Next
-            process(5) = ((process(5) + process(5) * posnow + Asc(Mid(src, posnow, 1) * i)) Mod 1000000000000) \ 2
-            process(5) = ((process(5) + process(5) * i + Asc(Mid(src, posnow, 1) * posnow)) Mod 1000000000000) \ 2
-            posnow = posnow Mod Len(src) + 1
-        Next
-        For i = 1 To 5
-            str = CStr(process(i)) + str
-        Next
-        For i = 5 To 45 Step 2
-            tmp = Val(Mid(str, i, 2))
-            tmp = tmp / 99 * 61
-            sfinal = sfinal + codes(tmp)
-        Next
-        TextBox2.Text = sfinal
+        rndpos = 1
+        psw = "123"
+        pospsw = 1
+        name = "filee"
+        pswlen = Len(psw)
+        pswhash = mhash.my_hash(psw)
+        poshash = 1
 
+
+        OpenFileDialog1.InitialDirectory = "D:"
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.ShowDialog()
+        fname_p = OpenFileDialog1.FileName
+        fname_s = OpenFileDialog1.SafeFileName
+        FileOpen(1, fname_p, OpenMode.Binary)
+        If System.IO.File.Exists(Application.StartupPath + "\tmp.txt") Then
+            Kill(Application.StartupPath + "\tmp.txt")
+        End If
+        FileOpen(2, "tmp.txt", OpenMode.Binary)
+        fileleng = FileLen(fname_p)
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = fileleng
+        ProgressBar1.Value = 0
+
+        ''''''''''''''''文件名加密输入''''''''''''''''''''
+
+        binval = Len(fname_s)
+        FilePut(2, binval)
+        For i = 1 To Len(fname_s)
+            chasc = Asc(Mid(fname_s, i, 1))
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            For j = 1 To 7
+                byte1(j) = byte1(j) Xor byte1(j - 1)
+
+            Next
+            binval = 0
+            For j = 0 To 7
+                binval = binval * 2 + byte1(j)
+            Next
+            FilePut(2, binval)
+        Next
+        '
+        ''''''''''''''随机数加密输入'''''''''''''''''''''
+        For i = 1 To 8
+            chasc = rndnum(i)
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            For j = 1 To 7
+                byte1(j) = byte1(j) Xor byte1(j - 1)
+            Next
+            binval = 0
+            For j = 0 To 7
+                binval = binval * 2 + byte1(j)
+            Next
+            FilePut(2, binval)
+        Next
+        '
+
+        ''''''''''''''文件加密输入''''''''''''''
+        fileleng = FileLen(fname_p)
+        For i = 1 To fileleng
+            FileGet(1, proc)
+
+            binstr = Dec2Bin(proc)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+
+            chasc = Asc(Mid(psw, pospsw, 1))
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte2(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            pospsw = pospsw Mod pswlen + 1
+
+
+            chasc = Asc(Mid(pswhash, poshash, 1))
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte3(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            poshash = poshash Mod 20 + 1
+
+
+            chasc = rndnum(rndpos)
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte4(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            rndpos = rndpos Mod 8 + 1
+
+            proc = 0
+            For j = 0 To 7
+                byte1(j) = byte1(j) Xor byte2(j)
+                byte1(j) = byte1(j) Xor byte3(j)
+                byte1(j) = byte1(j) Xor byte4(j)
+                proc = proc * 2 + byte1(j)
+            Next
+            FilePut(2, proc)
+            'ProgressBar1.Value = i
+            'Label1.Text = i
+        Next
+
+        FileClose(2)
+        FileClose(1)
+        'Kill(Application.StartupPath + "\tmp.txt")
     End Sub
+    Public Function Dec2Bin(dec As Integer) As String
+        Dim sum, tmp As String
+        sum = ""
+        Do While dec > 0 Or Len(sum) Mod 8 <> 0
+            tmp = CStr(dec Mod 2)
+            sum = tmp & sum
+            dec = dec \ 2
+        Loop
+        Return sum
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fname_p, fname_s, psw, pswhash, name As String
+        Dim chasc, rndnum(8), pswlen, byte1(7), byte2(7), byte3(7), byte4(7), binval, proc, pospsw, poshash, rndpos, namelen As Byte
+        Dim fileleng As Long
+        Dim binstr As String
+        Dim mhash As New myHash.myHash
+        FileOpen(1, "tmp.txt", OpenMode.Binary)
+
+        FileGet(1, namelen)
+
+        rndpos = 1
+        psw = "123"
+        pospsw = 1
+        pswlen = Len(psw)
+        pswhash = mhash.my_hash(psw)
+        poshash = 1
+
+        '''''''''''''''原文件名解密'''''''''''''''''''''
+        name = ""
+        For i = 1 To namelen
+            FileGet(1, binval)
+            binstr = Dec2Bin(binval)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            For j = 7 To 1 Step -1
+                If byte1(j) = byte1(j - 1) Then
+                    byte1(j) = 0
+                Else
+                    byte1(j) = 1
+                End If
+            Next
+            binval = 0
+            For j = 0 To 7
+                binval = binval * 2 + byte1(j)
+            Next
+            name = name & Chr(binval)
+        Next
+        FileOpen(2, name, OpenMode.Binary)
+
+        ''''''''''''''''''随机数解密'''''''''''''''''''''
+        For i = 1 To 8
+            FileGet(1, chasc)
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            For j = 7 To 1 Step -1
+                If byte1(j) = byte1(j - 1) Then
+                    byte1(j) = 0
+                Else
+                    byte1(j) = 1
+                End If
+            Next
+            binval = 0
+            For j = 0 To 7
+                binval = binval * 2 + byte1(j)
+            Next
+            rndnum(i) = binval
+        Next
+        ''''
+        '''''''''''''''文件解密''''''''''''''''''''''
+        fileleng = FileLen("tmp.txt")
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = fileleng
+        ProgressBar1.Value = 0
+        For i = Len(name) + 10 To fileleng
+            FileGet(1, proc)
+
+            binstr = Dec2Bin(proc)
+            For j = 0 To 7
+                byte1(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+
+            chasc = Asc(Mid(psw, pospsw, 1))
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte2(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            pospsw = pospsw Mod pswlen + 1
+
+            chasc = Asc(Mid(pswhash, poshash, 1))
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte3(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            poshash = poshash Mod 20 + 1
+
+            chasc = rndnum(rndpos)
+            binstr = Dec2Bin(chasc)
+            For j = 0 To 7
+                byte4(j) = Val(Mid(binstr, j + 1, 1))
+            Next
+            rndpos = rndpos Mod 8 + 1
+
+            proc = 0
+            For j = 0 To 7
+                byte1(j) = byte1(j) Xor byte4(j)
+                byte1(j) = byte1(j) Xor byte3(j)
+                byte1(j) = byte1(j) Xor byte2(j)
+                proc = proc * 2 + byte1(j)
+            Next
+            FilePut(2, proc)
+            ' ProgressBar1.Value = i
+        Next
+        FileClose(2)
+        FileClose(1)
+    End Sub
+
+
 End Class
+
