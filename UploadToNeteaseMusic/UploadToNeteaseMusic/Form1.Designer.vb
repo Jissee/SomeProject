1e6bd710596a2b93737ff80906c24bbbce889fad
俣桁 孙 <805084485@qq.com>
2020-09-20 12:46:23 +0800

    upd

---
+++ UploadToNeteaseMusic/UploadToNeteaseMusic/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(133, 65)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "转码"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 65)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "选择"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(252, 65)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 34)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "解码"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 111)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button3 As Button
+End Class
---
+++ UploadToNeteaseMusic/UploadToNeteaseMusic/Form1.vb
@@ -0,0 +1,116 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fhead(&H2B), bt(3) As Byte
+        Dim fname As String
+        Dim fileleng As Long
+        fname = Label1.Text
+        fhead(&H0) = Asc("R")
+        fhead(&H1) = Asc("I")
+        fhead(&H2) = Asc("F")
+        fhead(&H3) = Asc("F")
+
+        fhead(&H8) = Asc("W")
+        fhead(&H9) = Asc("A")
+        fhead(&HA) = Asc("V")
+        fhead(&HB) = Asc("E")
+        fhead(&HC) = Asc("f")
+        fhead(&HD) = Asc("m")
+        fhead(&HE) = Asc("t")
+        fhead(&HF) = Asc(" ")
+
+        fhead(&H10) = 16
+        fhead(&H11) = 0
+        fhead(&H12) = 0
+        fhead(&H13) = 0
+
+        fhead(&H14) = 1
+        fhead(&H15) = 0
+
+        fhead(&H16) = 1
+        fhead(&H17) = 0
+
+        fhead(&H18) = &H44
+        fhead(&H19) = &HAC
+        fhead(&H1A) = &H0
+        fhead(&H1B) = &H0
+
+
+        fhead(&H1C) = &H10
+        fhead(&H1D) = &HB1
+        fhead(&H1E) = &H2
+        fhead(&H1F) = &H0
+
+        fhead(&H20) = &H4
+        fhead(&H21) = &H0
+        fhead(&H22) = &H10
+        fhead(&H23) = &H0
+
+        fhead(&H24) = Asc("d")
+        fhead(&H25) = Asc("a")
+        fhead(&H26) = Asc("t")
+        fhead(&H27) = Asc("a")
+
+        fhead(&H28) = 0
+        fhead(&H29) = 0
+        fhead(&H2A) = 0
+        fhead(&H2B) = 0
+
+        Dim ifstream As IO.FileStream = New IO.FileStream(fname, IO.FileMode.Open)
+
+        fileleng = FileLen(fname)
+
+        For i = 0 To 3
+            fhead(&H28 + i) = fileleng Mod 256
+            fileleng = fileleng \ 256
+        Next
+
+        fileleng = FileLen(fname) + &H24
+
+        For i = 0 To 3
+            fhead(&H4 + i) = fileleng Mod 256
+            fileleng = fileleng \ 256
+        Next
+
+        Dim ofstream As IO.FileStream = New IO.FileStream(fname + ".wav", IO.FileMode.Create)
+
+        For i = 0 To &H2B
+            ofstream.WriteByte(fhead(i))
+        Next
+
+        fileleng = FileLen(fname)
+        For i = 1 To fileleng
+            ofstream.WriteByte(ifstream.ReadByte())
+        Next
+
+        ifstream.Close()
+        ofstream.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.Filter = "波形文件(*.wav)|*.wav|所有文件(*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+        Label1.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim fname As String
+        Dim offset, leng As Long
+        fname = Label1.Text
+        Dim ifstream As IO.FileStream = New IO.FileStream(fname, IO.FileMode.Open)
+        Dim ofstream As IO.FileStream = New IO.FileStream(Strings.Left(fname, Len(fname) - 4), IO.FileMode.Create)
+        For i = 0 To &H2B
+            offset = ifstream.ReadByte()
+        Next
+        leng = FileLen(fname)
+        offset = &H2C
+
+        Do While offset < leng
+            ofstream.WriteByte(ifstream.ReadByte())
+            offset = offset + 1
+        Loop
+        ifstream.Close()
+        ofstream.Close()
+
+    End Sub
+End Class
